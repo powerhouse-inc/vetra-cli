f15f019942b8467af79e36181f42b09e749a2c95
acaldas <acaldas@powerhouse.inc>
2026-07-16 13:34:56 +0100

    release: 0.0.6-dev.0

--- publish.config.js
+++ publish.config.js
@@ -3,7 +3,7 @@
 export default {
   groups: {
     'vetra': {
-      version: '0.0.1',
+      version: '0.0.6',
       packages: [
         { path: 'vetra-app', category: 'app' },
         { path: 'vetra-cli', category: 'cli' },
